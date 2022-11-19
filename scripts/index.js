54716d139f63cd2be1b4df7ff240ab6a011762ba
Hexteri-a <olliewells@thecooperschool.co.uk>
2022-11-19 16:57:53 +0000

    initial commit

---
+++ scripts/index.js
@@ -0,0 +1,18 @@
+const sections = document.querySelectorAll("section");
+const navLi = document.querySelectorAll("nav ul li");
+window.onscroll = () => {
+  var current = "";
+
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop;
+    if (pageYOffset >= sectionTop - 92) {
+      current = section.getAttribute("id"); }
+  });
+
+  navLi.forEach((li) => {
+    li.classList.remove("active");
+    if (li.classList.contains(current)) {
+      li.classList.add("active");
+    }
+  });
+};
